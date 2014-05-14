4fbc2bb7213d9e72b8da2671b0943a5721632d5c
anhallbe <anhallbe@gmail.com>
2014-05-14 18:52:23 +0200

    Added Probe events
    Handlers not implemented

---
+++ resourcemanager/src/main/java/resourcemanager/system/peer/rm/Probe.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package resourcemanager.system.peer.rm;
+
+import common.peer.AvailableResources;
+import se.sics.kompics.address.Address;
+import se.sics.kompics.network.Message;
+
+/**
+ *
+ * @author Andreas
+ */
+public class Probe {
+    public static class Request extends Message {
+        private int id;
+        
+        public Request(Address source, Address destination, int id) {
+            super(source, destination);
+            this.id = id;
+        }
+        
+        public int getId() {
+            return id;
+        }
+    }
+    
+    public static class Response extends Message {
+        private int id, cpu, mem;   //mem in MB
+        
+        public Response(Address source, Address destination, int id, int cpu, int mem) {
+            super(source, destination);
+            this.id = id;
+            this.cpu = cpu;
+            this.mem = mem;
+        }
+        
+        public int getId() {
+            return id;
+        }
+        
+        public int getCPU() {
+            return cpu;
+        }
+        
+        public int getMem() {
+            return mem;
+        }
+    }
+}
