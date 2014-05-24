b6e87ae4d9283bfc7dff7bb24155a72c33a2a83b
zhezha <zhezha@kth.se>
2014-05-25 01:36:41 +0200

    Made some modification for TMan

--- tman/src/main/java/tman/system/peer/tman/ComparatorByCPU.java
+++ tman/src/main/java/tman/system/peer/tman/ComparatorByCPU.java
@@ -21,20 +21,34 @@ public class ComparatorByCPU implements Comparator<PeerDescriptorTMan>{
         this.myDescriptor = myDescriptor;
     }
     
-     @Override
+    @Override
+    // In returned list after ordering, preferable peer should be placed to the head,
+    // If base node prefers o1 rather than o2, o1 should be placed before o2, i.e. o1 < o2.
     public int compare(PeerDescriptorTMan o1, PeerDescriptorTMan o2) {
-        int utility1 = o1.getAvailableResources().getNumFreeCpus();
-        int utility2 = o2.getAvailableResources().getNumFreeCpus();
+        int utility1, utility2;
         int myUtility = myDescriptor.getAvailableResources().getNumFreeCpus();
         
-        if (utility1 > myUtility && utility2 < myUtility) {
-            return 1;
+        if (o1.getAvailableResources() != null) {
+            utility1 = o1.getAvailableResources().getNumFreeCpus();
+        } else {
+            utility1 = -1;
         }
-        else if (utility1 < myUtility && utility2 > myUtility) {
+        if (o2.getAvailableResources() != null) {
+            utility2 = o2.getAvailableResources().getNumFreeCpus();
+        } else {
+            utility2 = -1;
+        }
+        // Prefer o1 than o2, o1 < o2.
+        if (utility1 > myUtility && utility2 < myUtility) {
             return -1;
         }
-        else if (Math.abs(utility1 - myUtility) < Math.abs(utility2 - myUtility)) {
+        // Prefer o2 than o1, o1 > o2.
+        else if (utility1 < myUtility && utility2 > myUtility) {
             return 1;
+        }
+        // Prefer o1 than o2, o1 < o2.
+        else if (Math.abs(utility1 - myUtility) < Math.abs(utility2 - myUtility)) {
+            return -1;
         }
         else if (utility1 == utility2) {
             return 0;
